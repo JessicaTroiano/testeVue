1b3ccbfe145da6a1604ee156b7107d6b05d7f8db
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2022-03-16 23:13:58 -0300

    alterar

---
+++ src/views/InicioNovo.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container> 
+    <div class="inicio-novo"></div>
+
+        <h2 class="text-h6 white--text text-center text-darken mb-3 mt-5"> Brasileirão Série A </h2>
+        <v-img 
+        class= 'imagem-inicial'
+        src='https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2974821:1596806515/Serie-A.png'>
+        </v-img>
+        <p class="text-justify mb-3 mt-5">
+            A Série A do Campeonato Brasileiro de Futebol de 2020, oficialmente Brasileirão Assaí – Série A 2020 por motivos de patrocínio, é a 64.ª edição da principal divisão do futebol brasileiro. A disputa tem o mesmo regulamento dos anos anteriores, quando foi implementado o sistema de pontos corridos.
+        </p>
+    </v-container>
+
+</template>
+
+<script>
+export default {
+
+    name: "InicioNovo"
+
+}
+</script>
+
+<style scoped>
+
+.nome{text-align: center;}
+
+
+</style>
